29cc7015d8c6347d2ce38741880cb8fbf4b029d1
Fabrice Gabriel Calixte <f.gabrielcalixte@gmail.com>
2018-02-20 16:23:41 +0100

    effacement du premier dossier racine

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Home;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class HomeController extends Controller
+{
+    /**
+     * Lists all home entities.
+     *
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $homes = $em->getRepository('AppBundle:Home')->findAll();
+
+        return $this->render('home/index.html.twig', array(
+            'homes' => $homes,
+        ));
+    }
+
+    /**
+     * Creates a new home entity.
+     *
+     * @Route("/new", name="home_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $home = new Home();
+        $form = $this->createForm('AppBundle\Form\HomeType', $home);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($home);
+            $em->flush();
+
+            return $this->redirectToRoute('home_show', array('id' => $home->getId()));
+        }
+
+        return $this->render('home/new.html.twig', array(
+            'home' => $home,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a home entity.
+     *
+     * @Route("/{id}", name="home_show")
+     * @Method("GET")
+     */
+    public function showAction(Home $home)
+    {
+        $deleteForm = $this->createDeleteForm($home);
+
+        return $this->render('home/show.html.twig', array(
+            'home' => $home,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing home entity.
+     *
+     * @Route("/{id}/edit", name="home_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Home $home)
+    {
+        $deleteForm = $this->createDeleteForm($home);
+        $editForm = $this->createForm('AppBundle\Form\HomeType', $home);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('home_edit', array('id' => $home->getId()));
+        }
+
+        return $this->render('home/edit.html.twig', array(
+            'home' => $home,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a home entity.
+     *
+     * @Route("/{id}", name="home_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Home $home)
+    {
+        $form = $this->createDeleteForm($home);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($home);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('home_index');
+    }
+
+    /**
+     * Creates a form to delete a home entity.
+     *
+     * @param Home $home The home entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Home $home)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('home_delete', array('id' => $home->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/Sketch.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Sketch
+ *
+ * @ORM\Table(name="sketch")
+ * @Vich\Uploadable()
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SketchRepository")
+ */
+class Sketch
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Assert\File(
+     *     maxSize="1M",
+     *     maxSizeMessage="Le fichier ne doit pas faire plus de 1Mo",
+     *     mimeTypes={"image/png", "image/jpeg"},
+     *     mimeTypesMessage="Veuillez mettre un format .jpeg ou .png"
+     *)
+     *
+     * @Vich\UploadableField(mapping="skech_image", fileNameProperty="imageName")
+     *
+     *
+     * @var File
+     */
+    private $imageFile;
+
+    //     * @Assert\Expression("this.getImageName()", message="Vous devez envoyer une image.")
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @var string
+     */
+    private $imageName;
+
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Sketch
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /*
+    * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+    *
+    * @return Partner
+    */
+
+    /**
+     * @return File|null
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get imageName
+     *
+     * @return string
+     */
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * Set imageName
+     *
+     * @param string $imageName
+     *
+     * @return Sketch
+     */
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Sketch
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+
+}
