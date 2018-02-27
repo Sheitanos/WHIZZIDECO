b707ffdea9d7981b8308db0f09e81f601ba653e4
sheitanos <nicklarson45@gmail.com>
2018-02-28 00:41:19 +0100

    debut presta+contact

---
+++ src/AppBundle/Controller/PrestationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class PrestationController extends controller {
+
+    /**
+     * @Route("/prestation", name="prestation_index")
+     */
+    public function indexAction()
+    {
+
+        return $this->render('prestation/prestations.html.twig'
+        );
+    }
+}
