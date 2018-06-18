9afe55d67f1352585361ffa551cb02542af4743e
fabien <fabien.poher45@gmail.com>
2018-06-18 16:11:52 +0200

    FosUser

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,27 +1,27 @@
 <?php
 
+
 namespace AppBundle\Entity;
 
-use FOS\UserBundle\Model\User as FosUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
-  * @ORM\Entity
-  * @ORM\Table(name="fos_user")
-  */
-class User extends FosUser
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
 {
     /**
-      * @ORM\Id
-      * @ORM\Column(type="integer")
-      * @ORM\GeneratedValue(strategy="AUTO")
-      */
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     protected $id;
 
     public function __construct()
-      {
-          parent::__construct;
-      }
+    {
+        parent::__construct();
+        // your own logic
+    }
 }
-
-?>
